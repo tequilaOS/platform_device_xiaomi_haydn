70dbc495c7d4bf52b87c7c396d81594a3fcc6e60
Bartłomiej Rudecki <ejbtrd@gmail.com>
2023-06-19 16:56:44 +0200

    haydn: Time for tequila!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_haydn.mk
+    $(LOCAL_DIR)/tequila_haydn.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_haydn-user \
-    lineage_haydn-userdebug \
-    lineage_haydn-eng
+    tequila_haydn-user \
+    tequila_haydn-userdebug \
+    tequila_haydn-eng
